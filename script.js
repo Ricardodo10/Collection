1a39e8da73f9c73bbf4acf62004a04238ba74dfc
ricardodo10 <ricardocoutinhosilva1999@gmail.com>
2022-04-21 09:47:02 +0200

    fin array

--- script.js
+++ script.js
@@ -1,30 +1,73 @@
 const collection = [
     {
-        name: "Ezreal"
-        position: "Adc"
-        civilization: "Piltover"
-        picture:"https://liquipedia.net/commons/images/a/ac/WR_Infobox_Ezreal.jpg"
-        
+        name: "Ezreal",
+        position: "Adc",
+        civilization: "Piltover",
+        picture:"https://liquipedia.net/commons/images/a/ac/WR_Infobox_Ezreal.jpg",
+        winrate: "49,67%"
+
     }
     {
-        name: "Jhin"
-        position: "Adc"
-        civilization: "Ionian prisonner"
-        picture: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Jhin_0.jpg"
+        name: "Jhin",
+        position: "Adc",
+        civilization: "Ionian prisonner",
+        picture: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Jhin_0.jpg",
+        winrate: "50,32%"
     }
     {
-        name: "Zeri"
-        position: "Adc"
-        civilization: "Zaun"
-        picture: "https://www.dexerto.com/wp-content/uploads/2022/01/04/zeri-splash.jpg"
+        name: "Zeri",
+        position: "Adc",
+        civilization: "Zaun",
+        picture: "https://www.dexerto.com/wp-content/uploads/2022/01/04/zeri-splash.jpg",
+        winrate: "44,15%"
     }
     {
-        name: "Lucian"
-        position: "Adc"
-        civilization: "Demacia"
-        picture: "https://static.wikia.nocookie.net/leagueoflegends/images/d/db/Lucian_OriginalCentered.jpg/revision/latest/scale-to-width-down/1280?cb=20180414203357"
+        name: "Lucian",
+        position: "Adc",
+        civilization: "Demacia",
+        picture: "https://static.wikia.nocookie.net/leagueoflegends/images/d/db/Lucian_OriginalCentered.jpg/revision/latest/scale-to-width-down/1280?cb=20180414203357",
+        winrate: "51,91%"
     }
     {
-        name:
+        name: "Xayah",
+        position: "Adc",
+        civilization: "Lhotlan",
+        picture: "https://liquipedia.net/commons/images/f/fb/League_Infobox_Xayah.jpg",
+        winrate: "50,85%"
+    }
+    {
+        name: "Vayne",
+        position: "Adc",
+        civilization: "Demacia",
+        picture: "https://liquipedia.net/commons/images/9/99/League_Infobox_Vayne.jpg",
+        winrate: "51,41%"
+    }
+    {
+        name: "Viego",
+        position: "Jungler",
+        civilization: "A lost Kingdom",
+        picture: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Viego_0.jpg",
+        winrate: "48,43%"
+    }
+    {
+        name: "Singed",
+        position: "Top",
+        civilization: "Zaun",
+        picture: "https://images.contentstack.io/v3/assets/blt187521ff0727be24/blt84220fdc67ead8d8/60ee120e2202166087e382c8/Singed_0.jpg",
+        winrate: "54,14%"
+    }
+    {
+        name: "Tryndamere",
+        position: "Top",
+        civilization: "Freljord",
+        picture: "https://images.contentstack.io/v3/assets/blt187521ff0727be24/bltd450a0f61fb2974c/60ee130f496a232835c1ffff/Tryndamere_0.jpg",
+        winrate: "52,75%"
+    }
+    {
+        name: "Gwen",
+        position: "Top",
+        civilization: "Iles Obscures",
+        picture: "https://images.contentstack.io/v3/assets/blt187521ff0727be24/bltd2d5de6c83b3349f/60ee0ddefdc6795ca5848472/gwen-splash.jpg",
+        winrate: "50,88%"
     }
 ]
