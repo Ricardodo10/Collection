719d768cb42fd7540bdb5c97bf94848698121480
ricardodo10 <ricardocoutinhosilva1999@gmail.com>
2022-04-21 09:03:00 +0200

    start vendredi

---
+++ script.js
@@ -0,0 +1,30 @@
+const collection = [
+    {
+        name: "Ezreal"
+        position: "Adc"
+        civilization: "Piltover"
+        picture:"https://liquipedia.net/commons/images/a/ac/WR_Infobox_Ezreal.jpg"
+        
+    }
+    {
+        name: "Jhin"
+        position: "Adc"
+        civilization: "Ionian prisonner"
+        picture: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Jhin_0.jpg"
+    }
+    {
+        name: "Zeri"
+        position: "Adc"
+        civilization: "Zaun"
+        picture: "https://www.dexerto.com/wp-content/uploads/2022/01/04/zeri-splash.jpg"
+    }
+    {
+        name: "Lucian"
+        position: "Adc"
+        civilization: "Demacia"
+        picture: "https://static.wikia.nocookie.net/leagueoflegends/images/d/db/Lucian_OriginalCentered.jpg/revision/latest/scale-to-width-down/1280?cb=20180414203357"
+    }
+    {
+        name:
+    }
+]
