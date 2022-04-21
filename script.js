f08fa8fcb25506cbbcd9c23b3a2079690f2f7584
ricardodo10 <ricardocoutinhosilva1999@gmail.com>
2022-04-21 13:37:37 +0200

    almost end

--- script.js
+++ script.js
@@ -1,73 +1,115 @@
-const collection = [
+let collection = [
     {
         name: "Ezreal",
         position: "Adc",
         civilization: "Piltover",
-        picture:"https://liquipedia.net/commons/images/a/ac/WR_Infobox_Ezreal.jpg",
+        picture:"",
         winrate: "49,67%"
 
-    }
+    },
     {
         name: "Jhin",
         position: "Adc",
         civilization: "Ionian prisonner",
-        picture: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Jhin_0.jpg",
+        picture: "",
         winrate: "50,32%"
-    }
+    },
     {
         name: "Zeri",
         position: "Adc",
         civilization: "Zaun",
-        picture: "https://www.dexerto.com/wp-content/uploads/2022/01/04/zeri-splash.jpg",
+        picture: "",
         winrate: "44,15%"
-    }
+    },
     {
         name: "Lucian",
         position: "Adc",
         civilization: "Demacia",
-        picture: "https://static.wikia.nocookie.net/leagueoflegends/images/d/db/Lucian_OriginalCentered.jpg/revision/latest/scale-to-width-down/1280?cb=20180414203357",
+        picture: "",
         winrate: "51,91%"
-    }
+    },
     {
         name: "Xayah",
         position: "Adc",
         civilization: "Lhotlan",
-        picture: "https://liquipedia.net/commons/images/f/fb/League_Infobox_Xayah.jpg",
+        picture: "",
         winrate: "50,85%"
-    }
+    },
     {
         name: "Vayne",
         position: "Adc",
         civilization: "Demacia",
-        picture: "https://liquipedia.net/commons/images/9/99/League_Infobox_Vayne.jpg",
+        picture: "",
         winrate: "51,41%"
-    }
+    },
     {
         name: "Viego",
         position: "Jungler",
         civilization: "A lost Kingdom",
-        picture: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Viego_0.jpg",
+        picture: "",
         winrate: "48,43%"
-    }
+    },
     {
         name: "Singed",
         position: "Top",
         civilization: "Zaun",
-        picture: "https://images.contentstack.io/v3/assets/blt187521ff0727be24/blt84220fdc67ead8d8/60ee120e2202166087e382c8/Singed_0.jpg",
+        picture: "",
         winrate: "54,14%"
-    }
+    },
     {
         name: "Tryndamere",
         position: "Top",
         civilization: "Freljord",
-        picture: "https://images.contentstack.io/v3/assets/blt187521ff0727be24/bltd450a0f61fb2974c/60ee130f496a232835c1ffff/Tryndamere_0.jpg",
+        picture: "",
         winrate: "52,75%"
-    }
+    },
     {
         name: "Gwen",
         position: "Top",
         civilization: "Iles Obscures",
-        picture: "https://images.contentstack.io/v3/assets/blt187521ff0727be24/bltd2d5de6c83b3349f/60ee0ddefdc6795ca5848472/gwen-splash.jpg",
+        picture: "",
         winrate: "50,88%"
     }
 ]
+
+let body = document.querySelector('body')
+let header0 = document.createElement ("header")
+body.appendChild(header0)
+let h1 = document.createElement('h1')
+let h2 = document.createElement('h2')
+
+header0.appendChild(h1);
+header0.appendChild(h2);
+h1.innerHTML="collection"
+h2.innerHTML = "my favorite league of legends champs"
+let main = document.createElement("main")
+body.appendChild(main)
+
+for (let i = 0; i < collection.length; i++) {
+    let article = document.createElement('article');
+    main.appendChild(article);
+    let img = document.createElement('img')
+    img.innerHTML = collection[i].picture;
+    let header  =document.createElement('header')
+    article.appendChild(header)
+    header.appendChild(img)
+
+    let div = document.createElement('div')
+    header.appendChild(div)
+    let h3 = document.createElement('h3')
+    div.appendChild(h3);
+    h3.innerHTML = collection[i].name
+      
+    let h4 = document.createElement('h4')
+    h4.innerHTML = collection[i].civilization
+    div.appendChild(h4)
+
+    let footer = document.createElement('footer')
+    let em = document.createElement('em');
+    em.innerHTML = collection[i].position;
+    footer.appendChild(em)
+    article.appendChild(footer)
+
+    let italic = document.createElement('i')
+    italic.innerHTML = collection[i].winrate;
+    footer.appendChild(italic);}
